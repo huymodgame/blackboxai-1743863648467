013a1e4b0b2ba63adfbbad10d0dde05151a6b6d0
huymodgame <116286563+huymodgame@users.noreply.github.com>
2025-04-05 21:34:15 +0700

    Add furina_ollama_chat/sw.js 1743863654281

---
+++ furina_ollama_chat/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'furina-ollama-chat-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/styles.css',
+  '/furina-bg.jpg',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
